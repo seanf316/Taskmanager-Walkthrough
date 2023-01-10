8f7cf3fce9854254d19743f919b31868b2ef5d50
seanf316 <seanfinn316@gmail.com>
2023-01-10 23:42:06 +0000

    Created the add task function and template

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // sidenav initialisation
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialisation
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: { done: "Select" }
+    });
+
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
